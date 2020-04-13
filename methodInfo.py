5627ea492a33a251f9741d40801e278980ddb438
BenHall-7 <benjaminjahall@gmail.com>
2020-04-13 19:39:56 -0400

    Adjust methodInfo logic to handle rogue symbols with no demname

--- methodInfo.py
+++ methodInfo.py
@@ -6,12 +6,15 @@ def loadMethodInfo(json):
     global methodInfo
     Method = namedtuple("Method", "name demname args")
     for s in json:
-        if s.bind == "GLOBAL" and s.vaddr > 0:
-            spl = s.demname.split('(')
-            if len(spl) == 1:
-                continue
-            name = spl[0]
-            args = []
-            for x in spl[1].split(')')[0].split(","):
-                args.append(x.strip())
-            methodInfo[s.vaddr] = Method(name, s.demname, args)
+        try:
+            if s.bind == "GLOBAL" and s.vaddr > 0:
+                spl = s.demname.split('(')
+                if len(spl) == 1:
+                    continue
+                name = spl[0]
+                args = []
+                for x in spl[1].split(')')[0].split(","):
+                    args.append(x.strip())
+                methodInfo[s.vaddr] = Method(name, s.demname, args)
+        except:
+            pass
